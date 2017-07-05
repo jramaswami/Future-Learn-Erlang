9c65a585be811fa6e395c7bd0375ed39f26fb457
jramaswami <jramaswami@gmail.com>
2017-07-05 09:10:04 -0400

    Instructor written template for rock, paper, scissors simulation.

--- rps.erl
+++ rps.erl
@@ -1,51 +1,135 @@
-% Future Learn :: Functional Programming in Erlang :: Modelling the basics of rock-paper-scissors
-% @author jramaswami
-
 -module(rps).
--include_lib("eunit/include/eunit.hrl").
--export([beat/1, lose/1, result/2, tournament/2]).
+-export([play/1,echo/1,play_two/3,rock/1,no_repeat/1,const/1,enum/1,cycle/1,rand/1,val/1,tournament/2]).
 
-beat(rock) ->
-    paper;
-beat(paper) ->
-    scissors;
-beat(scissors) ->
-    rock.
 
-lose(rock) ->
-    scissors;
-lose(scissors) ->
-    paper;
-lose(paper) ->
-    rock.
+%
+% play one strategy against another, for N moves.
+%
 
-% It is possible to use atoms, e.g. draw, win, lose, to represent
-% the result of the round.  However, using 0, 1, and -1 allows you
-% to use sum to easily get the required numerical result.
-result(Left, Left) ->
-    0;
-result(Left, Right) ->
-    case Left == beat(Right) of
-        true -> 1;
-        false -> -1 
+play_two(StrategyL,StrategyR,N) ->
+    play_two(StrategyL,StrategyR,[],[],N).
+
+% tail recursive loop for play_two/3
+% 0 case computes the result of the tournament
+
+% FOR YOU TO DEFINE
+% REPLACE THE dummy DEFINITIONS
+
+play_two(_,_,PlaysL,PlaysR,0) ->
+   dummy;
+
+play_two(StrategyL,StrategyR,PlaysL,PlaysR,N) ->
+   dummy.
+
+%
+% interactively play against a strategy, provided as argument.
+%
+
+play(Strategy) ->
+    io:format("Rock - paper - scissors~n"),
+    io:format("Play one of rock, paper, scissors, ...~n"),
+    io:format("... r, p, s, stop, followed by '.'~n"),
+    play(Strategy,[]).
+
+% tail recursive loop for play/1
+
+play(Strategy,Moves) ->
+    {ok,P} = io:read("Play: "),
+    Play = expand(P),
+    case Play of
+	stop ->
+	    io:format("Stopped~n");
+	_    ->
+	    Result = result(Play,Strategy(Moves)),
+	    io:format("Result: ~p~n",[Result]),
+	    play(Strategy,[Play|Moves])
     end.
 
-tournament(Lefts, Rights) ->
-    lists:sum(lists:zipwith(fun result/2, Lefts, Rights)).
+%
+% auxiliary functions
+%
 
-%%%%%%%%%%%%%%%%%%%%%% Tests %%%%%%%%%%%%%%%%%%%%%%%%%%
-result_test() ->
-    [?assert(result(rock, rock) == 0),
-     ?assert(result(paper, paper) == 0),
-     ?assert(result(scissors, scissors) == 0),
-     ?assert(result(rock, paper) == -1),
-     ?assert(result(paper, scissors) == -1),
-     ?assert(result(scissors, rock) == -1),
-     ?assert(result(rock, scissors) == 1),
-     ?assert(result(scissors, paper) == 1),
-     ?assert(result(paper, rock) == 1)
-    ].
+% transform shorthand atoms to expanded form
+    
+expand(r) -> rock;
+expand(p) -> paper;		    
+expand(s) -> scissors;
+expand(X) -> X.
 
-tournament_test() ->
-    [?assert(tournament([rock,rock,paper,paper],[rock,paper,scissors,rock]) == -1)
-    ].
+% result of one set of plays
+
+result(rock,rock) -> draw;
+result(rock,paper) -> lose;
+result(rock,scissors) -> win;
+result(paper,rock) -> win;
+result(paper,paper) -> draw;
+result(paper,scissors) -> lose;
+result(scissors,rock) -> lose;
+result(scissors,paper) -> win;
+result(scissors,scissors) -> draw.
+
+% result of a tournament
+
+tournament(PlaysL,PlaysR) ->
+    lists:sum(
+      lists:map(fun outcome/1,
+		lists:zipwith(fun result/2,PlaysL,PlaysR))).
+
+outcome(win)  ->  1;
+outcome(lose) -> -1;
+outcome(draw) ->  0.
+
+% transform 0, 1, 2 to rock, paper, scissors and vice versa.
+
+enum(0) ->
+    rock;
+enum(1) ->
+    paper;
+enum(2) ->
+    scissors.
+
+val(rock) ->
+    0;
+val(paper) ->
+    1;
+val(scissors) ->
+    2.
+
+% give the play which the argument beats.
+
+beats(rock) ->
+    scissors;
+beats(paper) ->
+    rock;
+beats(scissors) ->
+    paper.
+
+%
+% strategies.
+%
+echo([]) ->
+     paper;
+echo([Last|_]) ->
+    Last.
+
+rock(_) ->
+    rock.
+
+
+
+% FOR YOU TO DEFINE
+% REPLACE THE dummy DEFINITIONS
+
+no_repeat([]) ->
+    dummy;
+no_repeat([X|_]) ->
+    dummy.
+
+const(Play) ->
+    dummy.
+
+cycle(Xs) ->
+    dummy.
+
+rand(_) ->
+    dummy.
